36dc72b0491c536a3232b02b0cc692aa539e3408
Javier Calartrava <javiercalatrava@capitole-consulting.com>
2024-12-10 18:50:13 +0100

    Blank iOS App project

---
+++ SequencerPattern/SequencerPatternApp.swift
@@ -0,0 +1,17 @@
+//
+//  SequencerPatternApp.swift
+//  SequencerPattern
+//
+//  Created by Javier Calatrava on 10/12/24.
+//
+
+import SwiftUI
+
+@main
+struct SequencerPatternApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
